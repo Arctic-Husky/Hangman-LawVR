e20ac32c71f9929ecc88db0f5de00a436b28eba6
Pedro Brando <ph00.ramos@gmail.com>
2021-03-10 07:39:51 -0300

    Spawner, inimigos e vidas
    Implementação, sem gameover ainda portanto sistema rudimentar

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Spawner spawner;
+    [SerializeField]
+    public static int enemySpeed = 5;
+    // Update is called once per frame
+
+    void Start(){
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(Vector3.left * enemySpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+    if (col.gameObject.tag == "Player"){
+            ScoreVidas.vidas--;
+            spawner.SpawnInimigo();
+            Destroy (this.gameObject);
+            //reduzir a vida do Player
+        }
+    }
+}
