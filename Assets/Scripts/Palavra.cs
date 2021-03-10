5bffcf5cb6f94873cfbb9fc2c6f20ce459be8d35
gabcordeiro <61151121+gabcordeiro@users.noreply.github.com>
2021-03-10 06:49:20 -0300

    InitialCommit

---
+++ Assets/Scripts/Palavra.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Palavra", menuName ="Nova Palavra")]
+public class Palavra : ScriptableObject
+{
+    public string palavra;
+    public string descricao;
+}
